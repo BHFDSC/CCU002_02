cdd8e1c00b559f5151a96beefb57bf7545f2ee25
Hiu Yan Samantha Ip <hyi20@cam.ac.uk>
2021-06-24 15:40:21 +0100

    Update CCU002_02-R-si_fit_model_reducedcovariates_interactions.R

--- code/R/CCU002_02-R-si_fit_model_reducedcovariates_interactions.R
+++ code/R/CCU002_02-R-si_fit_model_reducedcovariates_interactions.R
@@ -5,7 +5,7 @@
 ## Thanks to Angela Wood, Tom Bolton and Tom Palmer for suggestions
 ## Contact detail: hyi20@medschl.cam.ac.uk
 ## =============================================================================
-source("/mnt/efs/hyi20/dars_nic_391419_j3w9t_collab/CCU002_vac/scripts/si_fit_get_data_surv.R")
+source("/mnt/efs/hyi20/dars_nic_391419_j3w9t_collab/CCU002_vac/scripts/CCU002_02-R-si_fit_get_data_surv.R")
 library(AER)
 library(stringi)  
 library(stringr) 
